a8662250945093240d25fb5d0005108e65dc9833
Steven Christe <steven.d.christe@nasa.gov>
2014-02-05 15:55:03 -0500

    first commit with SAS software for imperx camera support

---
+++ Makefile
@@ -0,0 +1,63 @@
+# Pattern matching in make files
+#   $^ matches all the input dependencies
+#   $< matches the first input dependency
+#   $@ matches the output
+
+CC=g++
+GCC_VERSION_GE_43 := $(shell expr `g++ -dumpversion | cut -f2 -d.` \>= 3)
+
+PUREGEV_ROOT = /opt/pleora/ebus_sdk
+OPENCVDIR = /usr/include/opencv2/
+CCFITSDIR = /usr/include/CCfits/
+INCLUDE = -I$(OPENCVDIR) -I$(PUREGEV_ROOT)/include/ -I$(CCFITSDIR)
+
+CFLAGS = -Wall $(INCLUDE) -Wno-unknown-pragmas
+ifeq "$(GCC_VERSION_GE_43)" "1"
+    CFLAGS += -std=gnu++0x
+endif
+
+IMPERX =-L$(PUREGEV_ROOT)/lib/		\
+	-lPvBase             		\
+	-lPvDevice          		\
+	-lPvBuffer          		\
+	-lPvPersistence      		\
+	-lPvGenICam          		\
+	-lPvStreamRaw        		\
+	-lPvStream 
+OPENCV = -lopencv_core -lopencv_highgui -lopencv_imgproc
+THREAD = -lpthread
+CCFITS = -lCCfits
+ifeq "$(GCC_VERSION_GE_43)" "1"
+    CCFITS += -lrt
+endif
+
+EXEC_CORE = snap
+EXEC_ALL = $(EXEC_CORE) 
+
+default: $(EXEC_CORE)
+
+all: $(EXEC_ALL)
+
+snap: snap.cpp ImperxStream.o compression.o
+	$(CC) $(CFLAGS) $^ -o $@ $(OPENCV) $(CCFITS) $(IMPERX)
+
+#This pattern matching will catch all "simple" object dependencies
+%.o: %.cpp %.hpp
+	$(CC) -c $(CFLAGS) $< -o $@
+
+install: $(EXEC_CORE)
+	#sudo systemctl stop sas
+	#sudo systemctl stop sbc_info
+	#sudo systemctl stop sbc_shutdown
+	#sudo systemctl stop relay_control
+	#sudo cp sunDemo /usr/local/bin/sas.runtime
+	#sudo cp sbc_info /usr/local/bin/sbc_info.runtime
+	#sudo cp sbc_shutdown /usr/local/bin/sbc_shutdown.runtime
+	#sudo cp relay_control /usr/local/bin/relay_control.runtime
+	#sudo systemctl start relay_control
+	#sudo systemctl start sbc_shutdown
+	#sudo systemctl start sbc_info
+	#sudo systemctl start sas
+
+clean:
+	rm -rf *.o *.out $(EXEC_ALL)
---
+++ snap.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <signal.h>
+#include <unistd.h>     /* for sleep()  */
+
+#include "ImperxStream.hpp"
+#include "compression.hpp"
+
+#define TIMEOUT 20000 // milliseconds
+#define SLEEP_CAMERA_CONNECT   1 // waits for errors while connecting to camera
+
+long localExposure = 1000;
+int16_t localPreampGain = -3;
+uint16_t localAnalogGain = 300;
+
+cv::Mat localFrame;
+HeaderData localHeader;
+
+sig_atomic_t volatile g_running = 1;
+
+void sig_handler(int signum)
+{
+    if ((signum == SIGINT) || (signum == SIGTERM))
+    {
+        if (signum == SIGINT) std::cerr << "Keyboard interrupt received (press enter)\n";
+        if (signum == SIGTERM) std::cerr << "Termination signal received (press enter)\n";
+        g_running = 0;
+    }
+}
+
+void ImageSave()
+{
+    if(!localFrame.empty())
+    {
+        char timestamp[14];
+        char filename[128];
+        struct tm *capturetime;
+        capturetime = gmtime(&localHeader.captureTime.tv_sec);
+        strftime(timestamp,14,"%y%m%d_%H%M%S",capturetime);
+
+        sprintf(filename, "image_%s_%03d.fits", timestamp, (int)(localHeader.captureTime.tv_nsec/1000000l));
+
+        printf("Saving image %s: exposure %d us, analog gain %d, preamp gain %d, min %d, max %d\n", filename, localHeader.exposure, localHeader.analogGain, localHeader.preampGain, localHeader.imageMinMax[0], localHeader.imageMinMax[1]);
+        writeFITSImage(localFrame, localHeader, filename);
+    }
+    else
+    {
+        std::cerr << "Empty image!\n";
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    switch(argc) {
+        case 5:
+            localPreampGain = atoi(argv[4]);
+        case 4:
+            localAnalogGain = atoi(argv[3]);
+        case 3:
+            localExposure = atoi(argv[2]);
+        case 2:
+            break;
+        default:
+            std::cout << "Calling sequeunce: snap <camera IP> [exposure time (us)] [analog gain (0-1023)] [preamp gain (-3,0,3,6)]\n";
+            return 0;
+    }
+
+    // to catch a Ctrl-C or termination signal and clean up
+    signal(SIGINT, &sig_handler);
+    signal(SIGTERM, &sig_handler);
+
+    bool cameraReady;
+    ImperxStream camera;
+
+    int width, height;
+
+    cameraReady = false;
+    while(g_running)
+    {
+        if (!cameraReady)
+        {
+            if (camera.Connect(argv[1]) != 0)
+            {
+                std::cerr << "Error connecting to camera!\n";
+                sleep(SLEEP_CAMERA_CONNECT);
+                continue;
+            }
+            else
+            {
+                camera.ConfigureSnap();
+
+                camera.SetExposure(localExposure);
+                camera.SetAnalogGain(localAnalogGain);
+                camera.SetPreAmpGain(localPreampGain);
+
+                width = camera.GetROIWidth();
+                height = camera.GetROIHeight();
+                localFrame.create(height, width, CV_8UC1);
+                if(camera.Initialize() != 0)
+                {
+                    std::cerr << "Error initializing camera!\n";
+                    //may need disconnect here
+                    sleep(SLEEP_CAMERA_CONNECT);
+                    continue;
+                }
+                cameraReady = true;
+            }
+        }
+        else
+        {
+            if(!camera.Snap(localFrame, TIMEOUT))
+            {
+                memset(&localHeader, 0, sizeof(HeaderData));
+
+                timespec now;
+                clock_gettime(CLOCK_REALTIME, &now);
+
+                localHeader.captureTime = now;
+
+                uint8_t localMin, localMax;
+
+                localHeader.exposure = localExposure;
+                localHeader.preampGain = localPreampGain;
+                localHeader.analogGain = localAnalogGain;
+
+                double tempMin, tempMax;
+                cv::minMaxLoc(localFrame, &tempMin, &tempMax);
+                std::cout << "Image max: " << (int)tempMax << std::endl;
+
+                calcMinMax(localFrame, localMin, localMax);
+                localHeader.imageMinMax[0] = localMin;
+                localHeader.imageMinMax[1] = localMax;
+
+                ImageSave();
+            }
+            else
+            {
+                std::cerr << "Image not taken\n";
+            }
+        }
+
+        std::cout << "Press return to snap\n";
+
+        std::string buffer;
+
+        std::getline(cin, buffer);
+    }
+
+    camera.Stop();
+    camera.Disconnect();
+
+    return 0;
+}
