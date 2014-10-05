7246d52ab2c6e4f55f3c46d337588623f9eee19e
Steven Christe <steven.d.christe@nasa.gov>
2014-10-04 22:49:23 -0400

    display now full screen, using glut

--- display.cpp
+++ display.cpp
@@ -1,60 +1,27 @@
-/*
- * Copyright (C) 1999-2001  Brian Paul   All Rights Reserved.
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * BRIAN PAUL BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
- * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-/* $XFree86: xc/programs/glxgears/glxgears.c,v 1.4 2003/10/24 20:38:11 tsi Exp $ */
-
-/*
- * This is a port of the infamous "gears" demo to straight GLX (i.e. no GLUT)
- * Port by Brian Paul  23 March 2001
- *
- * Command line options:
- *    -info      print GL implementation information
- *
- */
-
-
-#include <math.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
 #include <stdlib.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <X11/Xlib.h>
-#include <X11/keysym.h>
-#include <GL/gl.h>
-#include <GL/glx.h>
-#include <GL/glu.h>
-#include <stdint.h>
 #include <time.h>
-#include <GLUT/glut.h>
-
-#define BENCHMARK
-
-#ifdef BENCHMARK
-
-/* XXX this probably isn't very portable */
-
 #include <sys/time.h>
-#include <unistd.h>
+
+//angle of rotation
+static char message[1024];
+
+// to compile g++ test2.cpp -lGLU -lm -lGL -lglut 
+
+GLuint texture[1];      	// Storage for one texture ( NEW )
+
+// width and height of IMPERX Camera frame
+int width = 1296;
+int height = 966;
+// should only grey scale data here
+unsigned char *data = new unsigned char[1296*966];
 
 /* return current time (in seconds) */
-static int
-current_time(void)
+static int current_time(void)
 {
    struct timeval tv;
    struct timezone tz;
@@ -62,229 +29,8 @@ current_time(void)
    return (int) tv.tv_sec;
 }
 
-#else /*BENCHMARK*/
-
-/* dummy */
-static int
-current_time(void)
-{
-   return 0;
-}
-
-#endif /*BENCHMARK*/
-
-
-
-#ifndef M_PI
-#define M_PI 3.14159265
-#endif
-
-GLuint texture[1];      	// Storage for one texture ( NEW )
-
-GLfloat xrot;	                // X rotation ( NEW )
-GLfloat yrot;		        // Y rotation ( NEW )
-GLfloat zrot;	        	// Z rotation ( NEW )
-
-unsigned char *data = new unsigned char[256*256*3];
-int width = 256;
-int height = 256;
-
-static void LoadGLTextures()    // Load bitmaps and convert to textures
-{
-    for( int i = 0; i < height * width * 3; i++){
-        data[i] = rand();
-    }    
-
-	// Typical texture generation using data from the bitmap
-	glBindTexture(GL_TEXTURE_2D, texture[0]);
-
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, height, width, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)data);
-
-    //glPixelStorei(GL_UNPACK_ALIGNMENT);    
-
-    // Typical texture generation using data from the bitmap
-    
-    // free the memory
-}
-
-static void draw(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear screen and depth buffer
-    glLoadIdentity();                       // Reset the current modelview matrix
-
-    glTranslatef(0.0f,0.0f,-9.0f);          // Move into the screen 5 units
-
-    //glRotatef(xrot,1.0f,0.0f,0.0f);		// Rotate on the X axis
-	//glRotatef(yrot,0.0f,1.0f,0.0f);		// Rotate on the Y axis
-	//glRotatef(zrot,0.0f,0.0f,1.0f);		// Rotate on the Z axis
-
-    //glColor3f(0.5f, 0.5f, 0.0f);
-    LoadGLTextures();
-    glBindTexture(GL_TEXTURE_2D, texture[0]);       // Select our texture
-   glBegin(GL_QUADS);
-		// Front face
-		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom left of the texture and quad
-		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom right of the texture and quad
-		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Top right of the texture and quad
-		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Top left of the texture and quad
-	glEnd();	
-}
-
-/* new window size or exposure */
-static void reshape(int width, int height)
-{
-   GLfloat h = (GLfloat) height / (GLfloat) width;
-
-   glViewport(0, 0, (GLint) width, (GLint) height);
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   glFrustum(-1.0, 1.0, -h, h, 5.0, 60.0);
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
-   glTranslatef(0.0, 0.0, -40.0);
-}
-
-static void init(void)
-{
-    srand(time(NULL));
-
-    LoadGLTextures();
-    glShadeModel(GL_SMOOTH);                // Enable smooth shading
-	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);   // Black background
-	glClearDepth(1.0f);                     // Depth buffer setup
-	glEnable(GL_DEPTH_TEST);                // Enables depth testing
-	glDepthFunc(GL_LEQUAL);                 // The type of depth testing to do
-	glEnable(GL_TEXTURE_2D);
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);      // Really nice perspective calculations
-	glGenTextures(1, &texture[0]);		// Create the texture
-    
-}
-
-/*
- * Create an RGB, double-buffered window.
- * Return the window and context handles.
- */
-static void make_window( Display *dpy, const char *name,
-             int x, int y, int width, int height,
-             Window *winRet, GLXContext *ctxRet)
-{
-   int attrib[] = { GLX_RGBA,
-		    GLX_RED_SIZE, 1,
-		    GLX_GREEN_SIZE, 1,
-		    GLX_BLUE_SIZE, 1,
-		    GLX_DOUBLEBUFFER,
-		    GLX_DEPTH_SIZE, 1,
-		    None };
-   int scrnum;
-   XSetWindowAttributes attr;
-   unsigned long mask;
-   Window root;
-   Window win;
-   GLXContext ctx;
-   XVisualInfo *visinfo;
-
-   scrnum = DefaultScreen( dpy );
-   root = RootWindow( dpy, scrnum );
-
-   visinfo = glXChooseVisual( dpy, scrnum, attrib );
-   if (!visinfo) {
-      printf("Error: couldn't get an RGB, Double-buffered visual\n");
-      exit(1);
-   }
-
-   /* window attributes */
-   attr.background_pixel = 0;
-   attr.border_pixel = 0;
-   attr.colormap = XCreateColormap( dpy, root, visinfo->visual, AllocNone);
-   attr.event_mask = StructureNotifyMask | ExposureMask | KeyPressMask;
-   mask = CWBackPixel | CWBorderPixel | CWColormap | CWEventMask;
-
-   win = XCreateWindow( dpy, root, 0, 0, width, height,
-		        0, visinfo->depth, InputOutput,
-		        visinfo->visual, mask, &attr );
-
-   /* set hints and properties */
-   {
-      XSizeHints sizehints;
-      sizehints.x = x;
-      sizehints.y = y;
-      sizehints.width  = width;
-      sizehints.height = height;
-      sizehints.flags = USSize | USPosition;
-      XSetNormalHints(dpy, win, &sizehints);
-      XSetStandardProperties(dpy, win, name, name,
-                              None, (char **)NULL, 0, &sizehints);
-   }
-
-   ctx = glXCreateContext( dpy, visinfo, NULL, True );
-   if (!ctx) {
-      printf("Error: glXCreateContext failed\n");
-      exit(1);
-   }
-
-   XFree(visinfo);
-
-   *winRet = win;
-   *ctxRet = ctx;
-}
-
-
-static void event_loop(Display *dpy, Window win)
-{
-   while (1) {
-      while (XPending(dpy) > 0) {
-         XEvent event;
-         XNextEvent(dpy, &event);
-         switch (event.type) {
-	 case Expose:
-            /* we'll redraw below */
-	    break;
-	 case ConfigureNotify:
-	    reshape(event.xconfigure.width, event.xconfigure.height);
-	    break;
-         case KeyPress:
-            {
-               char buffer[10];
-               int code;
-               code = XLookupKeysym(&event.xkey, 0);
-               if (code == XK_Left) {
-                  
-               }
-               else if (code == XK_Right) {
-                  
-               }
-               else if (code == XK_Up) {
-                  
-               }
-               else if (code == XK_Down) {
-                  
-               }
-               else {
-                  (void) XLookupString(&event.xkey, buffer, sizeof(buffer),
-                                    NULL, NULL);
-                  if (buffer[0] == 27) {
-                     /* escape */
-                     return;
-                  }
-               }
-            }
-         }
-      }
-
-        /* next frame */
-        //angle += 2.0;
-
-        draw();
-
-        glXSwapBuffers(dpy, win);
-
-      /* calc framerate */
-      {
+void framerate(void){
+ /* calc framerate */
          static int t0 = -1;
          static int frames = 0;
          int t = current_time();
@@ -301,59 +47,111 @@ static void event_loop(Display *dpy, Window win)
                    fps);
             t0 = t;
             frames = 0;
-         }
-      }
-   }
+            }
 }
 
 
-int
-main(int argc, char *argv[])
+static void LoadGLTextures()    // Load bitmaps and convert to textures
 {
-   Display *dpy;
-   Window win;
-   GLXContext ctx;
-   char *dpyName = NULL;
-   GLboolean printInfo = GL_FALSE;
-   int i;
+    for( int i = 0; i < height * width; i++){
+        data[i] = rand();
+    }    
 
-   for (i = 1; i < argc; i++) {
-      if (strcmp(argv[i], "-display") == 0) {
-         dpyName = argv[i+1];
-         i++;
-      }
-      else if (strcmp(argv[i], "-info") == 0) {
-         printInfo = GL_TRUE;
-      }
-   }
+	// Typical texture generation using data from the bitmap
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
 
-   dpy = XOpenDisplay(dpyName);
-   if (!dpy) {
-      printf("Error: couldn't open display %s\n", dpyName);
-      return -1;
-   }
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
 
-   make_window(dpy, "FOXSI SAAS", 0, 0, 300, 300, &win, &ctx);
-   XMapWindow(dpy, win);
-   glXMakeCurrent(dpy, win, ctx);
-   reshape(300, 300);
-   glutFullScreen(void)                    // make the window full screen
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, height, width, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, (GLvoid*)data);
 
+    //glPixelStorei(GL_UNPACK_ALIGNMENT);    
 
-   if (printInfo) {
-      printf("GL_RENDERER   = %s\n", (char *) glGetString(GL_RENDERER));
-      printf("GL_VERSION    = %s\n", (char *) glGetString(GL_VERSION));
-      printf("GL_VENDOR     = %s\n", (char *) glGetString(GL_VENDOR));
-      printf("GL_EXTENSIONS = %s\n", (char *) glGetString(GL_EXTENSIONS));
-   }
+    // Typical texture generation using data from the bitmap
+    
+    // free the memory
+}
 
-   init();
+void drawString( int x, int y, char *str ) {
+	glColor3f( 1.0f, 1.0f, 1.0f );
+	glRasterPos2i( x, y );
+	
+	for ( int i=0, len=strlen(str); i<len; i++ ) {
+		if ( str[i] == '\n' ) {
+			y -= 16;
+			glRasterPos2i( x, y );
+		} else {
+			glutBitmapCharacter( GLUT_BITMAP_HELVETICA_12, str[i] );
+		}
+	}
+}
 
-   event_loop(dpy, win);
+void init (void) {
+    glEnable (GL_DEPTH_TEST); //enable the depth testing
+    glShadeModel (GL_SMOOTH); //set the shader to smooth shader
+	glGenTextures(1, &texture[0]);		// Create the texture
+	glEnable(GL_TEXTURE_2D);
+    LoadGLTextures();
+}
 
-   glXDestroyContext(dpy, ctx);
-   XDestroyWindow(dpy, win);
-   XCloseDisplay(dpy);
+void display (void) {
+    glClearColor (0.0,0.0,0.0,1.0); //clear the screen to black
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear the color buffer and the depth buffer
+    glLoadIdentity();  
+        	    sprintf( message, "(testing! %lf, %lf)",  -12.0, 3.0);
+    drawString(-0.2, -0.9, message);
+     glTranslatef(0.0f,0.0f,-8.0f);          // Move into the screen 5 units
 
-   return 0;
+    LoadGLTextures();
+    glBindTexture(GL_TEXTURE_2D, texture[0]);       // Select our texture
+    float zoom_factor = 1/130.0;
+    float x0 = -width*zoom_factor/2.0;
+    float x1 = width*zoom_factor/2.0;
+    float y0 = -height*zoom_factor/2.0;
+    float y1 = height*zoom_factor/2.0;
+
+   glBegin(GL_QUADS);
+		// Front face
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x0, y0,  1.0f);	// Bottom left of the texture and quad
+		glTexCoord2f(1.0f, 0.0f); glVertex3f( x1, y0,  1.0f);	// Bottom right of the texture and quad
+		glTexCoord2f(1.0f, 1.0f); glVertex3f( x1,  y1,  1.0f);	// Top right of the texture and quad
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x0,  y1,  1.0f);	// Top left of the texture and quad
+	glEnd();
+
+	    glutSwapBuffers(); //swap the buffers
+	framerate();
+}
+
+void reshape (int w, int h) {
+    glViewport (0, 0, (GLsizei)w, (GLsizei)h); //set the viewport to the current window specifications
+    glMatrixMode (GL_PROJECTION); //set the matrix to projection
+
+    glLoadIdentity ();
+    gluPerspective (60, (GLfloat)w / (GLfloat)h, 1.0, 100.0); //set the perspective (angle of sight, width, height, , depth)
+    glMatrixMode (GL_MODELVIEW); //set the matrix back to model
+}
+
+void keyboard (unsigned char key, int x, int y) {
+    if (key=='q')
+    {
+        glutLeaveGameMode(); //set the resolution how it was
+        exit(0); //quit the program
+    }
+}
+
+int main (int argc, char **argv) {
+    glutInit (&argc, argv);
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_DEPTH); //set the display to Double buffer, with depth
+    //glutGameModeString('1024×768:32@75'); //the settings for fullscreen mode
+    glutEnterGameMode(); //set glut to fullscreen using the settings in the line above
+    init (); //call the init function
+    glutDisplayFunc (display); //use the display function to draw everything
+    glutIdleFunc (display); //update any variables in display, display can be changed to anyhing, as long as you move the variables to be updated, in this case, angle++;
+    glutReshapeFunc (reshape); //reshape the window accordingly
+
+    glutKeyboardFunc (keyboard); //check the keyboard
+    glutMainLoop (); //call the main loop
+    return 0;
 }
